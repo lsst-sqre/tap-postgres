dcd73921b4bd006825c360d841b4fe98115fcbfa
Christine Banek <cbanek@lsst.org>
2020-01-23 14:41:29 -0700

    [DM-23081] Have build script build db container
    Build the postgresql container with the build.sh script.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 (cd tap && gradle clean assemble javadoc build test)
 cp tap/build/libs/*.war docker/tap
 
-(cd docker/tap && docker build . -t lsstdax/tap-postgres:dev)
+(cd docker/tap && docker build . -t lsstdax/tap-postgres-server:dev)
+(cd docker/postgresql && docker build . -t lsstdax/tap-postgres-db:dev)
