b91bbb0fa21c23341fb093c2da5d52c5cf80d4f4
Christine Banek <cbanek@lsst.org>
2020-01-24 14:49:09 -0700

    [DM-23189] Add in a uws database
    Okay, first off, we add a docker/uws directory, that builds the uws
    backend DB just like the lsst-tap-service.  This is just a straight
    copy.  Add building of the container to build.sh and push.sh .
    
    Remove the SampleJobManager, and switch over to the JobManager that
    persists things in postgres in the container mentioned above.
    
    Configure context and web xml files to put use the new classes and
    configure the connection to the uws db.

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ cp tap/build/libs/tap-1.1.war docker/tap/tap.war
 
 (cd docker/tap && docker build . -t lsstdax/tap-postgres-server:dev)
 (cd docker/postgresql && docker build . -t lsstdax/tap-postgres-db:dev)
+(cd docker/uws && docker build . -t lsstdax/tap-postgres-uws:dev)
