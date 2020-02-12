a468d5ad2ba5cf59182b2f7df564d5da94c67717
Christine Banek <cbanek@lsst.org>
2020-02-12 16:35:52 -0700

    [DM-23421] Rename tap_schema.obscore -> ivoa.obscore
    Give it the right name and move it over.  Add appropriate grants
    and permissiony type things.

--- docker/postgresql/tap-schema/0000_users.sql
+++ docker/postgresql/tap-schema/0000_users.sql
@@ -3,8 +3,10 @@
 
 CREATE USER tap_schema WITH ENCRYPTED PASSWORD 'pw-tapschema';
 CREATE DATABASE tap_schema WITH OWNER tap_schema;
+
 \c tap_schema
 CREATE SCHEMA tap_schema AUTHORIZATION tap_schema;
+CREATE SCHEMA ivoa AUTHORIZATION tap_schema;
 
 CREATE EXTENSION citext;
 CREATE EXTENSION pg_sphere;
--- docker/postgresql/tap-schema/9999_after.sql
+++ docker/postgresql/tap-schema/9999_after.sql
@@ -1,3 +1,4 @@
 \c tap_schema
 
 GRANT SELECT ON ALL TABLES IN SCHEMA tap_schema TO tap_schema;
+GRANT SELECT ON ALL TABLES IN SCHEMA ivoa TO tap_schema;
