c31e61134562c4dbfcea7089fc7e112b3f8cb7a7
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2025-09-09 08:24:55 -0700

    Change ResultServlet to generate result URL using gcs URL and bucket

--- tap/src/main/java/ca/nrc/cadc/sample/ResultsServlet.java
+++ tap/src/main/java/ca/nrc/cadc/sample/ResultsServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 /**
  * A servlet that handles redirecting to specific job results.
  * This servlet extracts the VOTable file name from the request path and constructs a URL to redirect the client.
@@ -15,8 +16,8 @@ import java.io.IOException;
  */
 public class ResultsServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(ResultsServlet.class);
+    private static final String bucket = System.getProperty("gcs_bucket");
     private static final String bucketURL = System.getProperty("gcs_bucket_url");
-
     /**
      * Processes GET requests by extracting the result filename from the request path and redirecting to the corresponding results URL.
      * The filename is assumed to be the path info of the request URL, following the first '/' character.
@@ -44,7 +45,14 @@ public class ResultsServlet extends HttpServlet {
      * @return the redirect URL constructed using the bucket URL and results file
      */
     private String generateRedirectUrl(String bucketUrlString, String path) {
-        String resultsFile  = path.substring(1);
-        return bucketUrlString + "/" + resultsFile;
+        String resultsFile = path.substring(1);
+        
+        if (bucket != null && !bucket.trim().isEmpty()) {
+            String encodedBucket = URLEncoder.encode(bucket, StandardCharsets.UTF_8);
+            return bucketUrlString + "/" + encodedBucket + "/" + resultsFile;
+        } else {
+            return bucketUrlString + "/" + resultsFile;
+        }
     }
+
 }
